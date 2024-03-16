2439549e5632b58d60cc57805ff96dd63a2a68dc
smialek <michal.smialek@ee.pw.edu.pl>
2024-03-16 10:30:04 +0100

    remove to src

--- view/presenters/PresentationDispatcher.tsx
+++
@@ -1,9 +0,0 @@
-import { ScreenId } from "../view_model/Types";
-import { Dispatch } from "react";
-
-export class PresentationDispatcher {
-  gUpdateView!: Dispatch<ScreenId>;
-  injectGlobalUpdateView(guv: Dispatch<ScreenId>) {
-    this.gUpdateView = guv;
-  }
-}
